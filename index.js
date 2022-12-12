bbaa08fe64d56a41d21441ed12ad269b3afd57b3
yurubahmed <yurubiftin2002@gmail.com>
2022-12-11 19:04:15 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber) {
+  if (someNumber <= 400){
+          return "This one is on me!";
+  }
+  else if (someNumber > 2000 && someNumber < 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if(someNumber > 2500){
+      return "No can do.";
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  if (city === "NYC") {
+      return "Ok, sounds good.";
+  }
+  else{
+      return "No go.";
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function  switchOnCharmFromTip (tip){
+  if (tip === "generous"){
+    return "Thank you so much.";}
+    else if (tip === "not as generous") {
+      return "Thank you."
+}
+else {
+  return "Bye."
+}
 }
